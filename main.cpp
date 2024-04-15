6b9940bc97805eec22aa64fb1804ac1a9a6fa0d8
Lakshya272 <lakshyajain272@gmail.com>
2024-04-15 10:39:13 +0530

    Main created

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+class transaction{
+    string coinName;
+    float rate;
+    float amount;
+    float total;
+    float balance;
+};
+
+int main(){
+    
+    return 0;
+}
